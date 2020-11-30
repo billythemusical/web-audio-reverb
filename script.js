b6450493d842346eb683982c4fdd8ef409572ead
Billy Bennett <bennett.billy@gmail.com>
2020-11-30 09:25:01 -0500

    createMediaStreamSource

--- script.js
+++ script.js
@@ -14,32 +14,18 @@ function initCapture() {
   navigator.mediaDevices
     .getUserMedia(constraints)
     .then(function(stream) {
-      /* Use the stream */
 
-      // Attach to our video object
-      audio.srcObject = stream;
-
-      // Wait for the stream to load enough to play
-      audio.onloadedmetadata = function(e) {
-
-        var audioCtx = new (window.AudioContext || window.webkitAudioContext);
-        var audioSource = audioCtx.createMediaElementSource(audio); // No longer playable by the normal audio tag
+        var ac = new AudioContext();
+        var audioSource = ac.createMediaStreamSource(stream);
 
         /* Using Tone.js */
-        Tone.setContext(audioCtx);
+        Tone.setContext(ac);
 
-        var reverb = new Tone.Reverb({
-          decay: 10,
-          wet: 0.5
-        });
+        var reverb = new Tone.Reverb({ decay: 10, wet: 0.5 });
 
         Tone.connect(audioSource, reverb);
         Tone.connect(reverb, Tone.getContext().destination);
-
-        // // Play the element
-        audio.play();
-
-      };
+        
     })
     .catch(function(err) {
       /* Handle the error */
